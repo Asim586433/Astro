18731b98419303518168c61ceb09dcae380396c0
Muhammad Asim <khuljasimsim586@gmail.com>
2025-12-14 05:07:43 +0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   site: 'https://your-domain.vercel.app',
-  output: 'server',
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-    imageService: true,
-    speedInsights: {
-      enabled: true,
-    },
-  }),
-  integrations: [tailwind()],
+  integrations: [tailwind()]
 });
